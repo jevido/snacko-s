591bc224fa7fdd3620d1005da18c5aa2bd3b3f04
= <=>
2022-12-20 16:42:16 +0100

    Added database setup

---
+++ src/routes/[room]/+page.server.js
@@ -0,0 +1,17 @@
+import db from '$lib/db';
+
+export async function load({ params }) {
+	let room = await db.collection('rooms').findOne({ name: params.room });
+	if (!room) {
+		return {
+			success: false,
+			reason: 'does not exist'
+		};
+	}
+
+	return {
+		success: trui,
+		room: room.name,
+		expires: room.expires_at
+	};
+}
---
+++ src/routes/[room]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+
+	const name = $page.params.room;
+	const roomdata = $page.data;
+</script>
+
+<h1>Coming someday</h1>
+{#if roomdata.success === false}
+	Room '{name}' {roomdata.reason}, might i suggest adding one?
+	{roomdata.reason}
+{:else}
+	Room '{name}' expires at: {roomdata.expires}
+{/if}
